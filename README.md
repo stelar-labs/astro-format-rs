1e5bd0ae23420d4042d9a546e505c351900675e8
Roy Okello <royokello@protonmail.com>
2023-10-14 23:00:26 +0300

    v2.0.0
    - changed encoding & decoding algorithm
    - added into bytes & try from bytes traits

--- README.md
+++ README.md
@@ -1,48 +1,70 @@
 # Astro Format
 
-Astro Format is a library for transcoding between multiple arrays and a single buffer.
+Astro Format is a library for transcoding between an array of implemented types and a single buffer.
 
-## Author
+## Authors
 
-Roy R. O. Okello
-
-[Email](mailto:royokello@protonmail.com)
-
-[Github](https://github.com/royokello)
-
-[Twitter](https://twitter.com/RealOkello)
+- Roy R. O. Okello: [Email](mailto:royokello@protonmail.com) & [GitHub](https://github.com/royokello)
 
 ## Usage
 
-### Cargo.toml
+### Installation
 
-```text
-[dependencies]
-astro-format = "1.2.0"
-```
-
-### Module.rs
-
-```text
-use astro_format;
-```
+- Using Crates by adding `astro-format = "2.0.0"` to `Cargo.toml` under `[dependencies]`
 
 ## API
 
 ### Encode
 
-```text
-let initial_arrays: Vec<&[u8]> = Vec::new();
+- `encode<T, I>(iterable: I) -> Result<Vec<u8>, Box<dyn Error>>`
+- Bounds -> T: IntoBytes, I: IntoIterator<Item = T>
 
-let encoded_buffer: Vec<u8> = astro_format::encode(&initial_arrays);
+```
+let list: Vec<&[u8]> = vec![&[1,2,3], &[4,5,6], &[7,8,9]];
+let encoded = astro_format::encode(list).unwrap();
 ```
 
 ### Decode
 
-```text
-let decoded_arrays: Vec<&[u8]> = astro_format::decode(&encoded_buffer)?;
+- `decode<'a, T, B>(buffer: &'a B) -> Result<Vec<T>, Box<dyn std::error::Error>>`
+- Bounds -> T: TryFromBytes<'a>, B: AsRef<[u8]>
+
 ```
+let decoded: Vec<&[u8]> = astro_format::decode(&encoded).unwrap();
+```
+
+### IntoBytes
+
+- `into_bytes(&self) -> Vec<u8>`
+- Support for Rust Data Types
+
+### TryFromBytes
+
+- `try_from_bytes(value: &'a [u8]) -> Result<Self, Box<dyn std::error::Error>>`
+- Support for Rust Data Types
 
 ## License
 
-MIT
+MIT License
+
+Copyright Stelar Labs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+## Disclaimer
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- src/lib.rs
+++ src/lib.rs
@@ -1,194 +1,318 @@
 
 use std::error::Error;
 
-pub fn encode(array: &[&[u8]]) -> Vec<u8> {
+pub fn encode<T, I>(iterable: I) -> Result<Vec<u8>, Box<dyn Error>>
+where
+    T: IntoBytes,
+    I: IntoIterator<Item = T>,
+{
 
-    if array.is_empty() {
+    let mut result: Vec<u8> = Vec::new();
 
-        vec![0_u8]
+    for item in iterable {
 
-    } else {
+        let item_buffer = item.into_bytes();
+
+        if item_buffer.len() > 4294967295 {
+            
+            result.push(8);
+            
+            result.extend((item_buffer.len() as u64).to_le_bytes());
         
-        array
-            .iter()
-            .map(|x| {
-                
-                let len: usize = x.len();
+        } else if item_buffer.len() > 65535 {
+            
+            result.push(4);
+            
+            result.extend((item_buffer.len() as u32).to_le_bytes());
+        
+        } else if item_buffer.len() > 255 {
+            
+            result.push(2);
 
-                if len == 0 {
+            result.extend((item_buffer.len() as u16).to_le_bytes());
 
-                    vec![0_u8, 0_u8]
+        } else if item_buffer.len() > 0 {
 
-                } else {
+            result.push(1);
 
-                    let mut len_buf = len.to_le_bytes().to_vec();
+            result.extend((item_buffer.len() as u8).to_le_bytes());
 
-                    while len_buf[len_buf.len() - 1] == 0 {
+        } else {
 
-                        len_buf.pop();
+            result.push(0);
 
-                    }
+        };
 
-                    len_buf.push(0);
+        result.extend(item_buffer)
 
-                    [len_buf, x.to_vec()].concat()
-
-                }
-
-            })
-            .fold(vec![], |acc, x| [acc, x].concat())
-    
     }
+
+    Ok(result)
+
 }
 
-pub fn decode(buffer: &[u8]) -> Result<Vec<&[u8]>, Box<dyn Error>> {
-    
-    let mut errors = false;
+pub fn decode<'a, T, B>(buffer: &'a B) -> Result<Vec<T>, Box<dyn Error>>
+where
+    T: TryFromBytes<'a>,
+    B: AsRef<[u8]>,
+{
 
-    let mut result: Vec<&[u8]> = Vec::new();
+    let buffer = buffer.as_ref();
 
-    let mut i: usize = 0;
+    let mut decoded_data = Vec::new();
+    let mut offset = 0;
 
-    let usize_bytes = (usize::BITS/8) as usize;
+    while offset < buffer.len() {
 
-    while i < buffer.len() {
+        let length_type = buffer[offset];
 
-        match buffer[i..].iter().position(|&x| x == 0_u8) {
-            
-            Some(res) => {
+        offset += 1;
 
-                if res == 0 {
+        let item_len: usize = match length_type {
+            0 => 0,
+            1 => buffer[offset] as usize,
+            2 => u16::from_le_bytes([
+                buffer[offset],
+                buffer[offset + 1],
+            ]) as usize,
+            4 => u32::from_le_bytes([
+                buffer[offset],
+                buffer[offset + 1],
+                buffer[offset + 2],
+                buffer[offset + 3],
+            ]) as usize,
+            8 => u64::from_le_bytes([
+                buffer[offset],
+                buffer[offset + 1],
+                buffer[offset + 2],
+                buffer[offset + 3],
+                buffer[offset + 4],
+                buffer[offset + 5],
+                buffer[offset + 6],
+                buffer[offset + 7],
+            ]) as usize,
+            _ => return Err("Invalid length type".into()),
+        };
 
-                    i += res;
+        offset += length_type as usize;
 
-                    let next_i = i + 1;
-
-                    if buffer.len() == 1 {
-                        break;
-                    } else if next_i < buffer.len() && buffer[next_i] == 0 {
-                        result.push(&[]); i += 2
-                    } else {
-                        errors = true; break
-                    }
-
-                } else {
-
-                    let mut size_buffer: Vec<u8> = buffer[i..res + i].to_vec();
-
-                    if size_buffer.len() > usize_bytes {
-                        errors = true;
-                        break
-                    } else {
-
-                        i += size_buffer.len();
-
-                        i += 1;
-
-                        while size_buffer.len() < usize_bytes {
-                            size_buffer.push(0);
-                        }
-                        
-                        let len: usize = usize::from_le_bytes(size_buffer.try_into().unwrap());
-
-                        result.push(&buffer[i..i + len]);
-    
-                        i += len;
-
-                    }
-                }
-            },
-
-            None => {
-                errors = true;
-                break
-            }
+        if offset + item_len <= buffer.len() {
+            let item_data = &buffer[offset..offset + item_len];
+            offset += item_len;
+            let item = T::try_from_bytes(item_data)?;
+            decoded_data.push(item);
+        } else {
+            return Err("Buffer is too short for item length".into());
         }
     }
 
-    if errors {
-
-        Err("Decode error!")?
-
-    } else {
-        
-        Ok(result)
-    
-    }
+    Ok(decoded_data)
 
 }
+
+pub trait IntoBytes {
+    fn into_bytes(&self) -> Vec<u8>;
+}
+
+macro_rules! impl_into_bytes_from_le_bytes {
+    ($type:ty) => {
+        impl IntoBytes for $type {
+            fn into_bytes(&self) -> Vec<u8> {
+                self.to_le_bytes().to_vec()
+            }
+        }
+        
+        impl<'a> IntoBytes for &$type {
+            fn into_bytes(&self) -> Vec<u8> {
+                (*self).to_le_bytes().to_vec()
+            }
+        }
+    };
+}
+
+impl_into_bytes_from_le_bytes!(u8);
+impl_into_bytes_from_le_bytes!(u16);
+impl_into_bytes_from_le_bytes!(u32);
+impl_into_bytes_from_le_bytes!(u64);
+impl_into_bytes_from_le_bytes!(u128);
+impl_into_bytes_from_le_bytes!(i8);
+impl_into_bytes_from_le_bytes!(i16);
+impl_into_bytes_from_le_bytes!(i32);
+impl_into_bytes_from_le_bytes!(i64);
+impl_into_bytes_from_le_bytes!(i128);
+
+macro_rules! impl_into_bytes_from_as_bytes {
+    ($type:ty) => {
+        impl IntoBytes for $type {
+            fn into_bytes(&self) -> Vec<u8> {
+                self.as_bytes().to_vec()
+            }
+        }
+        
+        impl<'a> IntoBytes for &$type {
+            fn into_bytes(&self) -> Vec<u8> {
+                (*self).as_bytes().to_vec()
+            }
+        }
+    };
+}
+
+impl_into_bytes_from_as_bytes!(String);
+impl_into_bytes_from_as_bytes!(str);
+
+macro_rules! impl_into_bytes_from_to_vec {
+    ($type:ty) => {
+        impl IntoBytes for $type {
+            fn into_bytes(&self) -> Vec<u8> {
+                self.to_vec()
+            }
+        }
+        
+        impl<'a> IntoBytes for &$type {
+            fn into_bytes(&self) -> Vec<u8> {
+                (*self).to_vec()
+            }
+        }
+    };
+}
+
+impl_into_bytes_from_to_vec!(Vec<u8>);
+impl_into_bytes_from_to_vec!([u8]);
+
+pub trait TryFromBytes<'a>: Sized {
+    fn try_from_bytes(value: &'a [u8]) -> Result<Self, Box<dyn std::error::Error>>;
+}
+
+impl TryFromBytes<'_> for String {
+    fn try_from_bytes(value: &[u8]) -> Result<Self, Box<dyn std::error::Error>> {
+        match String::from_utf8(value.to_vec()) {
+            Ok(s) => Ok(s),
+            Err(e) => Err(Box::new(e) as Box<dyn std::error::Error>),
+        }
+    }
+}
+
+impl TryFromBytes<'_> for Vec<u8> {
+    fn try_from_bytes(value: &[u8]) -> Result<Self, Box<dyn std::error::Error>> {
+        Ok(value.to_vec())
+    }
+}
+
+impl<'a> TryFromBytes<'a> for &'a [u8] {
+    fn try_from_bytes(value: &'a [u8]) -> Result<&'a [u8], Box<dyn std::error::Error>> {
+        Ok(value)
+    }
+}
+
+
+
+macro_rules! impl_try_from_bytes_for_numeric {
+    ($type:ty) => {
+        impl TryFromBytes<'_> for $type {
+            fn try_from_bytes(value: &[u8]) -> Result<Self, Box<dyn std::error::Error>> {
+                if value.len() != std::mem::size_of::<Self>() {
+                    return Err("Invalid byte size".into());
+                }
+                let mut array = [0u8; std::mem::size_of::<Self>()];
+                array.copy_from_slice(&value);
+                Ok(Self::from_le_bytes(array))
+            }
+        }
+    };
+}
+
+impl_try_from_bytes_for_numeric!(u8);
+impl_try_from_bytes_for_numeric!(u16);
+impl_try_from_bytes_for_numeric!(u32);
+impl_try_from_bytes_for_numeric!(u64);
+impl_try_from_bytes_for_numeric!(u128);
+impl_try_from_bytes_for_numeric!(i8);
+impl_try_from_bytes_for_numeric!(i16);
+impl_try_from_bytes_for_numeric!(i32);
+impl_try_from_bytes_for_numeric!(i64);
+impl_try_from_bytes_for_numeric!(i128);
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn errors() {
-        assert!(decode(&[20_u8, 20_u8]).is_err())
-    }
+    fn list_of_one() {
 
-    #[test]
-    fn one_array() {
+        let list = vec![vec![1, 2, 3]];
 
-        let arrays: Vec<&[u8]> = vec![&[1,2,3]];
+        let encoded = encode(&list).unwrap();
 
-        let buffer = encode(&arrays);
+        let decoded: Vec<Vec<u8>> = decode(&encoded).unwrap();
 
-        assert_eq!(arrays, decode(&buffer).unwrap());
+        assert_eq!(list, decoded);
 
     }
 
     #[test]
     fn three_arrays() {
 
-        let arrays: Vec<&[u8]> = vec![&[1,2,3], &[4,5,6], &[7,8,9]];
+        let list: Vec<&[u8]> = vec![&[1,2,3], &[4,5,6], &[7,8,9]];
 
-        let buffer = encode(&arrays);
+        let encoded = encode(list.clone()).unwrap();
 
-        assert_eq!(arrays, decode(&buffer).unwrap());
+        let exp_encoded = vec![1,3,1,2,3,1,3,4,5,6,1,3,7,8,9];
 
-    }
+        assert_eq!(exp_encoded, encoded);
 
-    #[test]
-    fn empty_array() {
+        let decoded: Vec<&[u8]> = decode(&encoded).unwrap();
 
-        let arrays = vec![];
-
-        let buffer = encode(&arrays);
-
-        assert_eq!(arrays, decode(&buffer).unwrap());
+        assert_eq!(list, decoded);
 
     }
 
     #[test]
-    fn one_empty_array() {
+    fn list_of_none() {
 
-        let arrays: Vec<&[u8]> = vec![&[]];
+        let list: Vec<u8> = vec![];
 
-        let buffer = encode(&arrays);
+        let encoded = encode(&list).unwrap();
 
-        assert_eq!(arrays, decode(&buffer).unwrap());
-
-    }
-
-    #[test]
-    fn three_empty_arrays() {
-
-        let arrays: Vec<&[u8]> = vec![&[], &[], &[]];
-
-        let buffer = encode(&arrays);
-
-        assert_eq!(arrays, decode(&buffer).unwrap());
+        assert_eq!(list, decode(&encoded).unwrap());
 
     }
 
     #[test]
-    fn two_arrays_and_empty_array() {
+    fn list_of_one_empty() {
 
-        let arrays: Vec<&[u8]> = vec![&[1,2,3], &[], &[7,8,9]];
+        let list: Vec<Vec<u8>> = vec![vec![]];
 
-        let buffer = encode(&arrays);
+        let encoded = encode(&list).unwrap();
 
-        assert_eq!(arrays, decode(&buffer).unwrap());
+        let decoded: Vec<Vec<u8>> = decode(&encoded).unwrap();
+
+        assert_eq!(list, decoded);
+
+    }
+
+    #[test]
+    fn list_of_three_empty() {
+
+        let list: Vec<&[u8]> = vec![&[], &[], &[]];
+    
+        let encoded = encode(list.clone()).unwrap();
+
+        let decoded: Vec<&[u8]> = decode(&encoded).unwrap();
+    
+        assert_eq!(list, decoded);
+
+    }
+
+    #[test]
+    fn list_of_two_and_one_empty() {
+
+        let list: Vec<&[u8]> = vec![&[1,2,3], &[], &[7,8,9]];
+
+        let encoded = encode(list.clone()).unwrap();
+
+        let decoded: Vec<&[u8]> = decode(&encoded).unwrap();
+    
+        assert_eq!(list, decoded);
 
     }
 
